3d8e077e0eef8cd660e65b561ddb8ec12445a812
Rafael Guedes <rafaelmagalhaes1@live.com>
2023-06-03 06:17:21 -0300

    Exercicio 2 concluído

--- fundamentos/sessao-3-introducao-a-javascript/dia-4-objetos/exercicio-objetos-2.js
+++ fundamentos/sessao-3-introducao-a-javascript/dia-4-objetos/exercicio-objetos-2.js
@@ -1,31 +1,6 @@
-# :dart: Dia 4 - Objetos
 
-## Exercícios :rocket:
+/*  Exercício 2
 
-### Exercício 1
-
-/*  
-    Crie uma variável chamada player e atribua a ela um objeto 
-    que reúna todas as informações das variáveis listadas.
-*/
-
-const name = 'Marta';
-const lastName = 'Silva';
-const age = 34;
-const medals = { golden: 2, silver: 3 };
-const bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
-
-const player = {
-  name: name,
-  lastName: lastName,
-  age: age,
-  medals: medals,
-  bestInTheWorld: bestInTheWorld,
-};
-
-###  Exercício 2
-
-/* 
     Acesse as chaves name, lastName e age por meio da sintaxe meuObjeto.chave, 
     concatene as informações e armazene na variável message no seguinte formato: 
     “A jogadora Marta Silva tem 34 anos de idade”.
