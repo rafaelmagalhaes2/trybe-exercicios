70a0ed6b0331ebb192b5820970cf0eb787cbc02d
Rafael <rafaelmagalhaes1@live.com>
2023-07-29 18:53:16 -0300

    feat: update css

--- front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-03-casa-de-cambio/src/app.js
+++ front-end/sessao-01-introducao-ao-frontend-e-javascript-assincrono/dia-03-casa-de-cambio/src/app.js
@@ -1,48 +1,35 @@
 // Função para fazer a requisição à API de forma assíncrona com async/await
 async function obterCotacao(moeda) {
-    try {
-        const response = await fetch(`https://api.exchangerate.host/latest?base=${moeda}`);
-        const data = await response.json();
-        exibirCotacao(data);
-    } catch (error) {
-        console.error('Erro ao obter cotação:', error);
-    }
+  try {
+    const response = await fetch(`https://api.exchangerate.host/latest?base=${moeda}`);
+    const data = await response.json();
+    exibirCotacao(data);
+  } catch (error) {
+    console.error('Erro ao obter cotação:', error);
+  }
 }
 
-// Função para exibir as cotações na tela em colunas
 function exibirCotacao(data) {
-    const cotacaoDiv = document.getElementById('cotacao');
-    cotacaoDiv.innerHTML = '';
+  const cotacaoDiv = document.getElementById('cotacao');
+  cotacaoDiv.innerHTML = '';
 
-    const cotacoes = data.rates;
-    for (const moeda in cotacoes) {
-        const valor = cotacoes[moeda].toFixed(2);
+  const cotacoes = data.rates;
+  const cotacoesHtml = Object.entries(cotacoes).map(([moeda, valor]) => `
+    <div class="card m-2">
+        <div class="card-body">
+            <h5 class="card-title">${moeda}</h5>
+            <p class="card-text">Valor: ${valor.toFixed(2)}</p>
+        </div>
+    </div>
+  `).join('');
 
-        const cardDiv = document.createElement('div');
-        cardDiv.className = 'card m-2';
-
-        const cardBodyDiv = document.createElement('div');
-        cardBodyDiv.className = 'card-body';
-
-        const cardTitle = document.createElement('h5');
-        cardTitle.className = 'card-title';
-        cardTitle.textContent = moeda;
-
-        const cardText = document.createElement('p');
-        cardText.className = 'card-text';
-        cardText.textContent = `Valor: ${valor}`;
-
-        cardBodyDiv.appendChild(cardTitle);
-        cardBodyDiv.appendChild(cardText);
-        cardDiv.appendChild(cardBodyDiv);
-
-        cotacaoDiv.appendChild(cardDiv);
-    }
+  cotacaoDiv.innerHTML = cotacoesHtml;
 }
 
 // Evento de submissão do formulário
-document.getElementById('conversaoForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const moeda = document.getElementById('moeda').value;
-    obterCotacao(moeda);
+const form = document.getElementById('conversaoForm');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const moeda = document.getElementById('moeda').value;
+  obterCotacao(moeda);
 });
