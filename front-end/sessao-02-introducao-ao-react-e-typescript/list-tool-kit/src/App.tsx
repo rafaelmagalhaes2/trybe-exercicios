cc520ad9eaabac78d454dfd5d6c41edc8c63005e
Rafael <rafaelmagalhaes1@live.com>
2023-08-21 09:56:18 -0300

    feat: add register item

--- front-end/sessao-02-introducao-ao-react-e-typescript/list-tool-kit/src/App.tsx
+++ front-end/sessao-02-introducao-ao-react-e-typescript/list-tool-kit/src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const toolKit = [
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'HTML',
+    'CSS',
+    'Node',
+    'Testes automatizados',
+  ];
+
+  const [index, setIndex] = useState(0);
+  const [toolList, setToolList] = useState(toolKit);
+  const [inputValue, setInputValue] = useState('');
+
+  function handleNextClick() {
+    if (index + 1 < toolList.length) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0); // Volta para o primeiro elemento
+    }
+  }
+
+  function handlePreviousClick() {
+    if (index - 1 >= 0) {
+      setIndex(index - 1);
+    } else {
+      setIndex(toolList.length - 1); // Vai para o último elemento
+    }
+  }
+
+  function handleAddClick() {
+    if (inputValue) {
+      setToolList([...toolList, inputValue]);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1>Caixa de ferramentas de uma Pessoa Desenvolvedora</h1>
+      <h2>{toolList[index]}</h2>
+      <section className="btn-next-prev">
+        <button onClick={handlePreviousClick}>Anterior</button>
+        <button onClick={handleNextClick}>Próximo</button>
+      </section>
+      <section className="input">
+        <h3>Adicione novas ferramentas:</h3>
+        <input onChange={({ target }) => setInputValue(target.value)} />
+        <button onClick={ handleAddClick }>Adicionar</button>
+      </section>
+      <section className="list">
+        <h3>Lista de itens do carrossel:</h3>
+        <ul>
+          {toolList.map((tool) => (
+            <li>{tool}</li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
