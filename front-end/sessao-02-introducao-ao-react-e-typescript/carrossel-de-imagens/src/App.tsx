07e7479a238f988d0c4e918098a1f57b677dc81a
Rafael <rafaelmagalhaes1@live.com>
2023-08-21 08:04:05 -0300

    feat: export images for images directory

--- front-end/sessao-02-introducao-ao-react-e-typescript/carrossel-de-imagens/src/App.tsx
+++ front-end/sessao-02-introducao-ao-react-e-typescript/carrossel-de-imagens/src/App.tsx
@@ -1,33 +1,11 @@
 import { useState } from 'react';
+import { images } from  './images/data';
 
 function App() {
-   const toolKit = [
-    {
-      name: 'Porto Seguro BA',
-      image: './src/assets/Passarela-do-Descobrimento.jpg',
-    },
-    {
-      name: 'Cidade Histórica',
-      image: './src/assets/cidade-historica-porto-seguro.jpg',
-    },
-    {
-      name: 'Porto Seguro BA - PSG',
-      image: './src/assets/conheca-porto-seguro.png',
-    },
-    {
-      name: 'Porto Seguro BA - PSG 2',
-      image: './src/assets/porto_seguro.jpg',
-    }, 
-    {
-      name: 'Porto Seguro BA - PSG 3',
-      image: './src/assets/porto_seguro.jpg',
-    }, 
-  ];
-
    const [index, setIndex] = useState(0);
 
 function handleNextClick() {
-  if (index + 1 < toolKit.length) {
+  if (index + 1 < images.length) {
     setIndex(index + 1);
   } else {
     setIndex(0); // Volta para o primeiro elemento
@@ -38,17 +16,17 @@ function handlePreviousClick() {
   if (index - 1 >= 0) {
     setIndex(index - 1);
   } else {
-    setIndex(toolKit.length - 1); // Vai para o último elemento
+    setIndex(images.length - 1); // Vai para o último elemento
   }
 }
 
  return (
    <main>
      <h1>Corrossel de imagens com REACT</h1>
-     <h2>{toolKit[index].name}</h2>
+     <h2>{images[index].name}</h2>
      <img
-        src={toolKit[index].image}
-        alt={toolKit[index].name}
+        src={images[index].image}
+        alt={images[index].name}
       /><br />
       <button onClick={handlePreviousClick}>Anterior</button>
       <button onClick={handleNextClick}>Próximo</button>
---
+++ front-end/sessao-02-introducao-ao-react-e-typescript/carrossel-de-imagens/src/images/data.tsx
@@ -0,0 +1,22 @@
+export const images = [
+    {
+        name: 'Porto Seguro BA',
+        image: './src/assets/Passarela-do-Descobrimento.jpg',
+    },
+    {
+        name: 'Cidade Histórica',
+        image: './src/assets/cidade-historica-porto-seguro.jpg',
+    },
+    {
+        name: 'Porto Seguro BA - PSG',
+        image: './src/assets/conheca-porto-seguro.png',
+    },
+    {
+        name: 'Porto Seguro BA - PSG 2',
+        image: './src/assets/porto_seguro.jpg',
+    }, 
+    {
+        name: 'Porto Seguro BA - PSG 3',
+        image: './src/assets/porto_seguro.jpg',
+    }, 
+];
