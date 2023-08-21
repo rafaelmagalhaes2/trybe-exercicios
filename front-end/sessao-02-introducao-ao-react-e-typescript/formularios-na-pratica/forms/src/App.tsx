962f3263f6651b54c2878df771cf62996b8e8a64
Rafael <rafaelmagalhaes1@live.com>
2023-08-21 14:42:23 -0300

    initial commit

---
+++ front-end/sessao-02-introducao-ao-react-e-typescript/formularios-na-pratica/forms/src/App.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [schooling, setSchooling] = useState('Médio');
+  const [resume, setResume] = useState('');
+  
+  return (
+    <>
+      <form>
+        <h1>Formulário</h1>
+        <div className="wrapper">
+          <label>
+            Nome
+            <input
+              value={name}
+              onChange={({ target }) => setName(target.value)}
+            />
+          </label>
+          <label>
+            E-mail
+            <input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+          </label>
+          <label>
+            Escolaridade
+            <select
+              value={schooling}
+              onChange={({ target }) => setSchooling(target.value)}
+            >
+              <option value="Médio">Médio</option>
+              <option value="Superior">Superior</option>
+              <option value="Pós-graduação">Pós-graduação</option>
+            </select>
+          </label>
+          <label>
+            Resumo das experiências
+            <textarea
+              value={resume}
+              onChange={({ target }) => setResume(target.value)}
+            />
+          </label>
+          <button>Enviar</button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default App;
+
