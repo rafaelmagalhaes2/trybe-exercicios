a2a0a9dc5fdd9e627c1ee609456445bc975b6fd1
Rafael <rafaelmagalhaes1@live.com>
2023-08-21 08:08:37 -0300

    feat: create component Carrossel

--- front-end/sessao-02-introducao-ao-react-e-typescript/carrossel-de-imagens/src/App.tsx
+++ front-end/sessao-02-introducao-ao-react-e-typescript/carrossel-de-imagens/src/App.tsx
@@ -1,36 +1,11 @@
-import { useState } from 'react';
-import { images } from  './images/data';
+import Carrossel from "./components/Carrossel";
 
 function App() {
-   const [index, setIndex] = useState(0);
-
-function handleNextClick() {
-  if (index + 1 < images.length) {
-    setIndex(index + 1);
-  } else {
-    setIndex(0); // Volta para o primeiro elemento
-  }
-}
-
-function handlePreviousClick() {
-  if (index - 1 >= 0) {
-    setIndex(index - 1);
-  } else {
-    setIndex(images.length - 1); // Vai para o último elemento
-  }
-}
 
  return (
-   <main>
-     <h1>Corrossel de imagens com REACT</h1>
-     <h2>{images[index].name}</h2>
-     <img
-        src={images[index].image}
-        alt={images[index].name}
-      /><br />
-      <button onClick={handlePreviousClick}>Anterior</button>
-      <button onClick={handleNextClick}>Próximo</button>
-     </main>
+    <main>
+      <Carrossel />
+    </main>
    );
  }
 
---
+++ front-end/sessao-02-introducao-ao-react-e-typescript/carrossel-de-imagens/src/components/Carrossel.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { images } from  '../images/data';
+
+function Carrossel() {
+  const [index, setIndex] = useState(0);
+
+  function handleNextClick() {
+    if (index + 1 < images.length) {
+        setIndex(index + 1);
+    } else {
+        setIndex(0); // Volta para o primeiro elemento
+    }
+  }
+
+  function handlePreviousClick() {
+    if (index - 1 >= 0) {
+        setIndex(index - 1);
+    } else {
+        setIndex(images.length - 1); // Vai para o último elemento
+    }
+  }
+
+  return (
+    <>
+      <h1>Corrossel de imagens com REACT</h1>
+      <h2>{images[index].name}</h2>
+      <img
+        src={images[index].image}
+        alt={images[index].name}
+      /><br />
+      <button onClick={handlePreviousClick}>Anterior</button>
+      <button onClick={handleNextClick}>Próximo</button>
+    </>
+  );
+}
+
+export default Carrossel;
