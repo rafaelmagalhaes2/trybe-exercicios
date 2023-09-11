bac2f3dab31e9461e091759919bb70750dabfaf2
Rafael <rafaelmagalhaes1@live.com>
2023-09-11 10:48:22 -0300

    arquivo de test criado

--- front-end/sessao-04-testes-automatizados-com-RTL/dia-01-introducao-a-RTL/projeto-rtl/src/App.test.tsx
+++ front-end/sessao-04-testes-automatizados-com-RTL/dia-01-introducao-a-RTL/projeto-rtl/src/App.test.tsx
@@ -1,10 +1,29 @@
 // src/App.test.tsx
 
 import { render, screen } from '@testing-library/react';
-
 import App from './App';
 
-it('should show "Vite + React" text', () => {
+test('Verifica se existe o campo Email.', () => {
   render(<App />);
-  expect(screen.getByText(/vite \+ react/i)).toBeInTheDocument();
-})
+  const inputEmail = screen.getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveProperty('type', 'email');
+});
+
+test('Verifica se existem dois botões', () => {
+  render(<App />);
+  const buttons = screen.getAllByRole('button');
+  expect(buttons).toHaveLength(2);
+});
+
+test('Verifica se existe um botão enviar na tela', () => {
+  render(<App />);
+  const btnSend = screen.getByTestId('id-send');
+  expect(btnSend).toBeInTheDocument();
+});
+
+test('Verifica se existe um botão voltar na tela', () => {
+  render(<App />);
+  const btnBack = screen.getByRole('button', { name: 'Voltar' });
+  expect(btnBack).toBeInTheDocument();
+});
