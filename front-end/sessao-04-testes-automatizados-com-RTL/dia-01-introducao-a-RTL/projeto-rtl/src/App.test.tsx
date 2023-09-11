0f720c75929f0a200717f627fff2d2c622e1ceac
Rafael <rafaelmagalhaes1@live.com>
2023-09-11 11:12:23 -0300

    fix: label Email:

--- front-end/sessao-04-testes-automatizados-com-RTL/dia-01-introducao-a-RTL/projeto-rtl/src/App.test.tsx
+++ front-end/sessao-04-testes-automatizados-com-RTL/dia-01-introducao-a-RTL/projeto-rtl/src/App.test.tsx
@@ -1,5 +1,8 @@
+// src/App.test.tsx
+
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
 test('Verifica se existe o campo Email.', () => {
