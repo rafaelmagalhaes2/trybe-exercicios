9f47ddbdedc6264661be75a9561fa14b1485cd8e
Rafael <rafaelmagalhaes1@live.com>
2023-09-11 11:26:30 -0300

    create ValidEmail.test.tsx

--- front-end/sessao-04-testes-automatizados-com-RTL/dia-01-introducao-a-RTL/projeto-rtl/src/App.tsx
+++ front-end/sessao-04-testes-automatizados-com-RTL/dia-01-introducao-a-RTL/projeto-rtl/src/App.tsx
@@ -1,45 +1,35 @@
-// src/App.tsx
-import './App.css';
-
 import { useState } from 'react';
+import ValidEmail from './components/ValidEmail';
 import './App.css';
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [saveEmail, setSaveEmail] = useState('');
-
-  const handleSendEmail = () => {
-    setSaveEmail(email);
+  const [email, setEmail] = useState('')
+  const [title, setTitle] = useState('')
+   function handleClick() {
+    setTitle(email);
     setEmail('');
-  };
-
+  }
+  
   return (
     <div className="App">
-      <div className="form-container">
-        <label htmlFor="id-email">
-          Email:
-          <input
-            id="id-email"
-            type="email"
-            onChange={ (e) => setEmail(e.target.value) }
-            value={ email }
+      <label htmlFor="id-email">
+        Email:
+         <input
+           id="id-email"
+           type="email" 
+           onChange={ ({target}) => setEmail(target.value)}
+           value={ email }
           />
         </label>
         <input
-          id="btn-send"
-          type="button"
-          data-testid="id-send"
-          value="Enviar"
-          onClick={ handleSendEmail }
+         id="btn-send"
+         type="button"
+         data-testid="id-send"
+         value="Enviar"
+         onClick={ handleClick }
         />
-        <input
-          id="btn-back"
-          type="button"
-          value="Voltar"
-          onClick={ () => setSaveEmail('') }
-        />
-      </div>
-      <h2>{ `Valor: ${saveEmail}` }</h2>
+      <input id="btn-back" type="button" value="Voltar" />
+      <ValidEmail email={ title } />
     </div>
   );
 }
