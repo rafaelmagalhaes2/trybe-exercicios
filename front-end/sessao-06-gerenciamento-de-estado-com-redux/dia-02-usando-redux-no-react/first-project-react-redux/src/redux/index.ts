78c7b842230d8b9ab8733e9a01add99f478c0849
Rafael <rafaelmagalhaes1@live.com>
2023-09-27 07:38:46 -0300

    feat: create reducer

--- front-end/sessao-06-gerenciamento-de-estado-com-redux/dia-02-usando-redux-no-react/first-project-react-redux/src/redux/index.ts
+++ front-end/sessao-06-gerenciamento-de-estado-com-redux/dia-02-usando-redux-no-react/first-project-react-redux/src/redux/index.ts
@@ -1,6 +1,15 @@
 // ./src/redux/index.ts
 import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-const store = createStore();
+type ActionType = {
+  type: string;
+}
+
+const INITIAL_STATE = { count: 0 };
+
+const reducer = (state = INITIAL_STATE, action: ActionType) => state;
+
+const store = createStore(reducer, composeWithDevTools());
 
 export default store;
