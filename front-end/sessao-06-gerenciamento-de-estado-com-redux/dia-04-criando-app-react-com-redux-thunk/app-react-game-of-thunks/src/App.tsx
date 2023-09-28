c840e0469183d993f3d54b26585d3cf09ffaccde
Rafael <rafaelmagalhaes1@live.com>
2023-09-28 16:38:19 -0300

    create App.tsx

--- front-end/sessao-06-gerenciamento-de-estado-com-redux/dia-04-criando-app-react-com-redux-thunk/app-react-game-of-thunks/src/App.tsx
+++ front-end/sessao-06-gerenciamento-de-estado-com-redux/dia-04-criando-app-react-com-redux-thunk/app-react-game-of-thunks/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.tsx\
+
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { fetchCharacter } from './redux/actions';
+import { Dispatch } from './types';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [character, setCharacter] = useState('');
+  const dispatch: Dispatch = useDispatch();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <input
+        type="text"
+        onChange={ (
+          { target }: React.ChangeEvent<HTMLInputElement>,
+        ) => setCharacter(target.value) }
+        value={ character }
+        placeholder="Character Name"
+      />
+      <button
+        type="button"
+        onClick={ () => dispatch(fetchCharacter(character)) }
+      >
+        Search
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
